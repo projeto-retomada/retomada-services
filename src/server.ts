7d45aa5ab92cea1fb08148f159ce75669f27a76e
Luis Otavio Bernardo de Andrade <luis291099@gmail.com>
2020-08-13 20:17:13 -0300

    primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const appRetomada = express();
+
+appRetomada.get('/', (request, response) => {
+    return response.send('200: Ok')
+});
+
+appRetomada.listen(3333);
