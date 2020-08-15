c8db61ed8c9a3f41707272ce962e9eb44994e00c
mirielesilverio <mirielesilverio@hotmail.com>
2020-08-15 18:29:31 -0300

    Criando migration de instituição

---
+++ src/database/migrations/01_create_instituicao.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('instituicao', table => {
+        table.increments('id_instituicao').primary();
+        table.string('emblema');
+        table.string('cnpj').notNullable();
+        table.string('nome').notNullable();
+        table.string('nome_normalizado').notNullable();
+        table.text('metadata').notNullable();
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('instituicao');
+}
